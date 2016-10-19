4499ee3cec76afd18273bdd54c4bad9398b2a6bd
avinashraz <avinashaninthaneni@gmail.com>
2016-10-19 08:52:59 +0530

    Sum of two numbers

---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Avinash
+ *
+ */
+public class SumOfTwoNumbers {
+
+	/**
+	 * 
+	 */
+	public SumOfTwoNumbers() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numberOne = 20;
+double numberTwo = 2.5678;
+int result;
+result = (int)(numberOne+numberTwo);
+System.out.println("The Sum Of Two Numbers Is " +result);
+
+	}
+
+}
+
